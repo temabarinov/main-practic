b952058bbef75e970fa8cfc4295c8dbe46c0bcfa
tema <artembarinov61@icloud.com>
2026-01-16 00:03:35 +0300

    1 zadanie

--- src/1.cpp
+++ src/1.cpp
@@ -1,10 +1,79 @@
 #include "common.h"
 
+enum months{
+    Январь=1,
+    Февраль,
+    Март,
+    Апрель,
+    Май,
+    Июнь,
+    Июль,
+    Август,
+    Сентябрь,
+    Октябрь,
+    Ноябрь,
+    Декабрь
+};
+
+
+
+
+
 
 int main(){
-    std::string a{};
-    a="sss";
-    std::cout<<a;
+    int n;
+    
+    
+    while (n!=0){
+        std::cout<<"Введите номер месяца"<<std::endl;
+        std::cin>>n;
+        switch (n)
+        {
+        case Январь:
+            std::cout<<"Январь"<<std::endl;;
+            break;
+        case Февраль:
+            std::cout<<"Февраль"<<std::endl;;
+            break;
+        case Март:
+            std::cout<<"Март"<<std::endl;;
+            break;
+        case Апрель:
+            std::cout<<"Апрель"<<std::endl;;
+            break;
+        case Май:
+            std::cout<<"Май"<<std::endl;;
+            break;
+        case Июнь:
+            std::cout<<"Июнь"<<std::endl;;
+            break;
+        case Июль:
+            std::cout<<"Июль"<<std::endl;;
+            break;
+        case Август:
+            std::cout<<"Август"<<std::endl;;
+            break;
+        case Сентябрь:
+            std::cout<<"Сентябрь"<<std::endl;;
+            break;
+        case Октябрь:
+            std::cout<<"Октябрь"<<std::endl;;
+            break;
+        case Ноябрь:
+            std::cout<<"Ноябрь"<<std::endl;;
+            break;
+        case Декабрь:
+            std::cout<<"Декабрь"<<std::endl;;
+            break;
+        case 0:
+            std::cout<<"До свидания";
+            break;
+           
+        default:
+            std::cout<<"Неправильный номер месяца"<<std::endl;
+            break;
+        }
+}
 
 
     return 0;
