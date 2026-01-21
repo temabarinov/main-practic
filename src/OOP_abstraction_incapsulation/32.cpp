5c18717d31a207b1b15a6b240d88d59285b06635
tema <artembarinov61@icloud.com>
2026-01-21 21:22:28 +0300

    change:counter_remake

--- src/OOP_abstraction_incapsulation/32.cpp
+++ src/OOP_abstraction_incapsulation/32.cpp
@@ -2,14 +2,14 @@
 
 class adress{
     private:
-    std::string city{};
-    std::string street{};
-    int num_house;
-    int num_apart;
+        std::string city{};
+        std::string street{};
+        int num_house;
+        int num_apart;
     public:
-    adress(std::string setcity, std::string setstreet,int setnum_house, int setnum_apart):
-    city {setcity}, street{setstreet}, num_house {setnum_house}, num_apart {setnum_apart}{}
-    
+        adress(std::string setcity, std::string setstreet,int setnum_house, int setnum_apart):
+        city {setcity}, street{setstreet}, num_house {setnum_house}, num_apart {setnum_apart}{}
+
 
 
 
--- src/class_and_objects/22.cpp
+++ src/class_and_objects/22.cpp
@@ -5,20 +5,15 @@ class counter{
     private:
     int first_number;
     public:
+    counter() : first_number(1) {}
     void setFirsrt_number(int First_number){
         first_number=First_number;
     }
     std::string operation{};
-    void f(){
-        if (operation=="+"){
-            first_number++;
-        }else if(operation=="-"){
-            first_number--;
-        }else if(operation=="="){
-            std::cout<<first_number<<std::endl;
-        }
-
-    }
+    void increment() { first_number++; }
+    void decrement() { first_number--; }
+    counter(int start) : first_number(start) {}
+    int getCounter(){return first_number;}
 
 
 
@@ -29,18 +24,29 @@ int main(int argc,char** argv){
     std::string ask{};
     std::cout<<"Вы хотите указать начаьное значение счётчика? Выберите да или нет:"<<std::endl;
     std::cin>>ask;
-    counter counter;
+    std::string operation{};
+
+    
     if (ask=="Да" || ask=="да"){
         int first_number;
         std::cout<<"Введите начальное значение счетчика:"<<std::endl;
         std::cin>>first_number;
-        
-        counter.setFirsrt_number(first_number);
+        counter ccounter(first_number);
 
-        while(counter.operation!="x"){
+
+        
+
+        while(operation!="x"){
             std::cout<<"Введите команду ('+', '-', '=' или 'x'):"<<std::endl;
-            std::cin>>counter.operation;
-            counter.f();
+            std::cin>>operation;
+            if (operation=="+"){
+                ccounter.increment();
+
+            }else if(operation=="-"){
+                ccounter.decrement();
+            }else if(operation=="="){
+                std::cout<<ccounter.getCounter()<<"\n";
+            }
 
         }
         std::cout<<"До свидания!"<<std::endl;
@@ -50,16 +56,21 @@ int main(int argc,char** argv){
 
 
     }else{
-        counter.setFirsrt_number(1);
-         while(counter.operation!="x"){
+        counter ccounter(1);
+         while(operation!="x"){
             std::cout<<"Введите команду ('+', '-', '=' или 'x'):"<<std::endl;
-            std::cin>>counter.operation;
-            counter.f();
+            if (operation=="+"){
+                ccounter.increment();
+
+            }else if(operation=="-"){
+                ccounter.decrement();
+            }else if(operation=="="){
+                std::cout<<ccounter.getCounter()<<"\n";
+            }
 
         }
         std::cout<<"До свидания!"<<std::endl;
     }
-
 
 
     return 0;
