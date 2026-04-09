885b09e4f366c2af62f4275015b01336366cc15e
tema <artembarinov61@icloud.com>
2026-04-09 21:20:13 +0300

    fibonacci_with_memo

--- src/algoritms/fibonacci.cpp
+++ src/algoritms/fibonacci.cpp
@@ -2,11 +2,20 @@
 
 #include "common.h"
 
-long long fibonacci(int n) {
+long long fibonacci_helper(int n, long long memo[]) {
   if (n <= 1) {
     return n;
   }
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  if (memo[n] != 0) {
+    return memo[n];
+  }
+  memo[n] = fibonacci_helper(n - 1, memo) + fibonacci_helper(n - 2, memo);
+  return memo[n];
+}
+
+long long fibonacci(int n) {
+  long long memo[100] = {0};
+  return fibonacci_helper(n, memo);
 }
 
 int main() {
